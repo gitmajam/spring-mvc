651237295aaf34d2832a4c3b79b7aac1f80a47fa
marlon <marlon@station2.station2>
2022-09-18 19:57:56 -0500

    added messages.properties

--- src/com/spring/mvc/Customer.java
+++ src/com/spring/mvc/Customer.java
@@ -9,22 +9,7 @@ import javax.validation.constraints.Size;
 import com.spring.mvc.validation.CourseCode;
 
 public class Customer {
-	
-	@CourseCode(value = {"Ref","Dat"}, message="must start with Ref or Dat")
-	private String courseCode;
-	
-	@NotNull(message = "is required")
-	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
-	private String postalCode;
 
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-	
 	private String firstName;
 
 	@NotNull(message = "is required")
@@ -35,22 +20,13 @@ public class Customer {
 	@Min(value = 0, message = "must be greather than or equal to zero")
 	@Max(value = 10, message = "must be less than or equal to 10")
 	private Integer freePasses;
-	
-	public String getCourseCode() {
-		return courseCode;
-	}
 
-	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
-	}
+//	@NotNull(message = "is required")
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+	private String postalCode;
 
-	public Integer getFreePasses() {
-		return freePasses;
-	}
-
-	public void setFreePasses(Integer freePasses) {
-		this.freePasses = freePasses;
-	}
+	@CourseCode(value = { "Ref", "Dat" }, message = "must start with Ref or Dat")
+	private String courseCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -66,6 +42,30 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 
 }
